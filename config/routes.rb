c851cd87af4366a42f6a4f4eab96dd2d25af43a3
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-05-03 23:05:28 -0500

    Adjustment to Groups and Entities

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  get '/welcome', to: 'welcome#index', as: 'welcome'
   get 'entities/new'
   get 'entities/destroy'
   get 'entities/edit'
   get 'entities/update'
   get 'entities/index'
-  devise_for :users, sign_out_via: [:get, :post]
-  resources :users do
+  devise_for :users, sign_out_via: [:get, :post], controllers: { registrations: "users/registrations" }
+  resources :users, only: [:index, :show, :destroy] do
     resources :groups do
       resources :entities
     end
@@ -13,5 +14,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
   # Defines the root path route ("/")
-  root "groups#index"
+  root 'groups#index'
 end
