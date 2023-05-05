2caad244ab77450b724a6d71aae027793e2af81d
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-05-05 01:04:59 -0500

    Update manifest.js

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,5 @@
 //= link_tree ../images
-//= link_directory ../../javascript .js
+//= link_tree ../../javascript .js
 //= link_directory ../stylesheets .css
 //= link application.js
 //= link application.css
