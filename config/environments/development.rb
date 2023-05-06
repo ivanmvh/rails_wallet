511fe0413c3e1cda19916e4bc24bf0e5150ebc1a
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-05-06 17:00:52 -0500

    Test change to css with config.assets.compile = false to verify if chan…

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # @im-20230506 - tratando solucionar problema css
+  config.assets.debug = false
+  config.assets.compile = true
 end
